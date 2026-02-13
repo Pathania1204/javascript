77c10574d1fb5e230a09e1a65353c2366499a34e
Pathania1204 <garimapathania23@gmail.com>
2026-02-13 11:59:28 +0530

    updated

---
+++ file28.js
@@ -0,0 +1,31 @@
+// const names = ["John", "Cathy", "Amy"]
+// names.forEach((a,b)=>{
+//     console.log(b);
+// })//will print indexes
+
+// const names = ["John", "Cathy", "Amy"]
+// names.forEach((name,index)=>{
+//     console.log(name);
+// })//will print names
+
+// const names = ["John", "Cathy", "Amy"]
+// names.forEach((name,index)=>{
+//     console.log(index,name);
+// })//will print indexes and names
+
+// const names = ["John", "Cathy", "Amy"]
+// names.forEach((name,index,arr)=>{
+//     console.log(arr);
+// })//will print arr in each loop 
+
+// const names = ["John", "Cathy", "Amy"]
+// names.forEach((name,index,arr)=>{
+//     console.log(arr[index]);
+// })//will print names 
+
+const names = ["John", "Cathy", "Amy"]
+const score = [56,78,90]
+names.forEach((name,index)=>{
+    console.log(name,score[index]);
+})//will print name with score
+
