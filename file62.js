9f0aec5c2398c7b2fbc1dfca3e9eaafa04a2eeeb
Pathania1204 <garimapathania23@gmail.com>
2026-02-18 14:53:35 +0530

    updated

---
+++ file62.js
@@ -0,0 +1,8 @@
+// let num = 0
+// //Nullish Opperator '??'
+// console.log(num || 10); //output 10(true)
+// console.log(num ?? 10); // output 0(false)
+
+let num 
+console.log(num || 10); 
+console.log(num ?? 10);
