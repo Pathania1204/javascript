75418d9e918407aa82c00bc0e25fc39fd6b50864
Pathania1204 <garimapathania23@gmail.com>
2026-02-18 14:48:27 +0530

    updated

--- file60.js
+++ file60.js
@@ -1,7 +1,11 @@
-const cart={
+const cart = {
     items : [],
-    addItems:"",
-    showItems:"",
+    addItems(item){
+        this.items.push(item)
+    },
+    showItems() {
+        console.log(this.items);
+    },
 };
 cart.addItems("Laptop"),
 cart.addItems("Desktop"),
